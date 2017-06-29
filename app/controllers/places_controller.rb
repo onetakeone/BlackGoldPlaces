4d1a81a7f27c87ab9662edce42874d848e5f7f39
N <limepassion@gmail.com>
2017-06-29 12:26:04 +0300

    ++index bgr img

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,13 @@ class PlacesController < ApplicationController
 
   before_action :authenticate_user!
   
+  def index
+    @place = Place.all 
+  end
+
   def new
   end
+
   def create
     @place = Place.new(place_params)
     if @place.valid?
@@ -11,8 +16,12 @@ class PlacesController < ApplicationController
     else
       render action: 'new'
     end
-    redirect_to home_index_path
-  end  
+    redirect_to places_path
+  end 
+
+  def show
+    @place = Place.find(params[:id])
+  end 
 
   def place_params
     params.require(:place).permit(:name, :city, :street, :bldg, :description, :tags)
